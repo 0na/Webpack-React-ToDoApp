33c2faf751bc365a88cb62057094926618a4879c
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-03-01 07:44:42 +0100

    init project

--- webpack.config.jsx
+++ webpack.config.jsx
@@ -1,30 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  mode: "delevopment",
-  // resolve: {
-  //   extensions: [".js", ".jsx"]
-  //},
-  entry: "./src/index.jsx",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "index.bundle.js"
+    filename: "app.bundle.js"
   },
-  //obiekt — module. Jego zadanie polega na wpływaniu na moduły, które bezpośrednio ładujemy jako zależności naszej aplikacji. Przeważnie jedynym parametrem, który będzie wpływał na moduły, będzie parametr rules.
   module: {
     rules: [
       {
-        test: /\.jsx$/,
-        //test — warunek, który musi zostać spełniony w nazwie importowanego modułu, aby mógł zostać przetransformowany danym loaderem. Najczęściej są to rozszerzenia (tak jak w przykładzie powyżej),
+        test: /\.js$/,
+        exclude: /node_modules/,
         loader: "babel-loader"
-        //options: {presets: ['env', 'react']} //zamiast w bliku .babelrc mozna to wpisac tu
       },
       {
         test: /\.css$/,
         use: [
           { loader: "style-loader" },
           {
-            loader: "css-loader", //npm npm install --save-dev style-loader css-loader
+            loader: "css-loader",
             options: {
               modules: true
             }
@@ -34,4 +28,3 @@ module.exports = {
     ]
   }
 };
-//Powyższy przykład testuje, czy ładowany moduł ma rozszerzenie .js, a następnie, jeśli warunek jest spełniony, transformuje go przy pomocy babel-loader.
