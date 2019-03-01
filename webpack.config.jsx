376dea9571d3e174f8dc56435e8c667d60960029
Aleksandra Garlikowska <garlikowska@gmail.com>
2019-03-01 10:29:32 +0100

    finish with configuration, plugins added

--- webpack.config.jsx
+++ webpack.config.jsx
@@ -1,41 +1,56 @@
 const path = require("path");
-
+//const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OptimizeJsPlugin = require("optimize-js-plugin");
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "src/index.html",
+    filename: "index.html",
+    inject: "body"
+  })
+];
 module.exports = env => {
-  const environment = env || "production";
-
-  return {
-    mode: environment,
-    // mode: env || 'production',
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "app." + environment + "app.bundle.js"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader"
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader",
-              options: {
-                modules: true
+  if (env === "production") {
+    plugins.push(
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    );
+    return {
+      mode: env || "production",
+      entry: "./src/index.js",
+      output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "app.bundle.js"
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: "style-loader"
+              },
+              {
+                loader: "css-loader",
+                options: {
+                  modules: true
+                }
               }
-            }
-          ]
-        }
-      ]
-    },
-    optimization: {
-      minimize: false // w trybie produkcyjnym kod jest domyślnie minifikowany. W trybie produkcyjnym jest domyślnie ustawione na true.
-    }
-  };
+            ]
+          }
+        ]
+      },
+      optimization: {
+        minimize: false // w trybie produkcyjnym kod jest domyślnie minifikowany. W trybie produkcyjnym jest domyślnie ustawione na true.
+        //      minimizer: [new UglifyJsPlugin()] -brzydki kod
+      },
+      plugins: plugins
+    };
+  }
 };
